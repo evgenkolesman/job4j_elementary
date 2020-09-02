05e3fce345825e9ddcd1521061fc95145ad39d32
glavs <glavstroi_e@mail.ru>
2020-09-02 14:28:41 +0400

    Загрузка файлов в github

---
+++ src/ru/job4j/Calculator.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Calculator {
+    public static void main(String[] args) {
+            System.out.println ()
+            int age = 18;
+            System.out.println ()
+            float length = 78.0332
+            System.out.println ()
+            boolean baby = true;
+            }
+}
