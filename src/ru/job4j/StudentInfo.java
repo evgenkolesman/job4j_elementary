0c1fbfa28cdc5309116b6a3f9ab4bcef2245668d
evgenkolesman <70563563+evgenkolesman@users.noreply.github.com>
2020-09-02 12:59:08 +0400

    Add files via upload

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Evgen Kolesnikov");
+        System.out.println("10.12.1987");
+
+}
+}
