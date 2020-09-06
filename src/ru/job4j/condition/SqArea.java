36c94035c26f5275348aac6eda5e5362b48a1ea1
glavs <glavstroi_e@mail.ru>
2020-09-06 10:34:57 +0400

    Загрузка файлов в github

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,8 +1,9 @@
 package ru.job4j.condition;
 
 public class SqArea {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static double square(int p, int k) {
-    double rsl = p/Math.pow(2*(k+1),2)*k;
+    double rsl = p / Math.pow(2 * (k + 1), 2) * k;
     return rsl;
 }
 
