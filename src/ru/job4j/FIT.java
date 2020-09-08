895176581fb7813e72344a9605fce3eb83aa83f3
glavs <glavstroi_e@mail.ru>
2020-09-08 11:22:43 +0400

    Загрузка файлов в github

--- src/ru/job4j/FIT.java
+++ src/ru/job4j/FIT.java
@@ -1,11 +1,11 @@
 package ru.job4j;
 public class FIT {
     public static double manWeight(short height) {
-        double rsl = ((height-100)*1.15);
+        double rsl = ((height - 100)*1.15);
         return rsl;
     }
     public static double womanWeight(short height) {
-        double rsl = ((height-110)*1.15);
+        double rsl = ((height - 110) * 1.15);
         return rsl;
     }
     public static void main(String[] args) {
