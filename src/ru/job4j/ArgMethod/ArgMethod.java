b194135be185fd0d88d52c53b0a3eef2bbbb9dda
glavs <glavstroi_e@mail.ru>
2020-09-08 11:19:01 +0400

    Загрузка файлов в github

--- src/ru/job4j/ArgMethod/ArgMethod.java
+++ src/ru/job4j/ArgMethod/ArgMethod.java
@@ -1,15 +1,10 @@
 package ru.job4j.ArgMethod;
-
 import static java.lang.System.*;
-
 public class ArgMethod {
-
     public static int func1(int x) {
-            int y = (x * x) + 1;
-
+        int y = (x * x) + 1;
         return y;
     }
-
         public static void main(String[] args) {
             int result = ArgMethod.func1(100);
             System.out.println(result);
