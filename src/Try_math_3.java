e0ab16895841c387d671be5ab66c1f7086bb8354
glavs <glavstroi_e@mail.ru>
2020-10-06 11:59:57 +0400

    Загрузка файлов в github

---
+++ src/Try_math_3.java
@@ -0,0 +1,12 @@
+public class Try_math_3 {
+    public static void main (String[] args)
+    {
+        double a = 9;
+        double b = 1;
+        double sqrt = Math.sqrt(a*b);
+        double pow = Math.pow (a,b);
+        System.out.println("Корень из произведения переменных равен "+ sqrt
+                +" a в степени b равно "+pow);
+    }
+
+}
