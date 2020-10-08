c7c168cd03e333ef81ba0f40b0612c1294578473
glavs <glavstroi_e@mail.ru>
2020-10-08 17:46:00 +0400

    Загрузка файлов в github

---
+++ src/Try4.java
@@ -0,0 +1,12 @@
+// запись с расшифровкой логических выражений "|| - и"
+
+public class Try4 {
+    public static void main {
+            int x
+            int y = 2;
+            x !=0 && 1/x> x+y; // не делить на ноль
+            x< y ? x:y; // дает меньшее из значений  x и y
+        System.out.println( "X ="+x);
+        }
+    }
+
