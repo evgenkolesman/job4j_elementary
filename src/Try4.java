d3305795ad20fa6403132e6ecad88adf888a76a9
glavs <glavstroi_e@mail.ru>
2020-10-08 17:55:37 +0400

    Загрузка файлов в github

--- src/Try4.java
+++ src/Try4.java
@@ -1,12 +1,18 @@
 // запись с расшифровкой логических выражений "|| - и"
 
 public class Try4 {
-    public static void main {
-            int x
-            int y = 2;
-            x !=0 && 1/x> x+y; // не делить на ноль
-            x< y ? x:y; // дает меньшее из значений  x и y
-        System.out.println( "X ="+x);
-        }
+    public static void main (String [] args){
+        //(int Ver {
+        //  int x
+        // int y = 2;
+        //  x != 0 && 1 / x > x + y; // не делить на ноль
+        //   x<y ?x:
+        //   y; // дает меньшее из значений  x и y
+        //  System.out.println("X =" + x);)
+
+    String a = "Evgen";
+    String b = a.substring (0,3);// выделяет от 0 до ... те буквы которые есть в пред аргументе
+    System.out.println(b);
     }
+}
 
