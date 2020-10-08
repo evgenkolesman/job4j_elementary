e2c5b1a9627f757618fe65d6108d7875bf42365c
glavs <glavstroi_e@mail.ru>
2020-10-08 18:19:57 +0400

    Загрузка файлов в github

--- src/Try4.java
+++ src/Try4.java
@@ -13,6 +13,14 @@ public class Try4 {
     String a = "Evgen";
     String b = a.substring (0,3);// выделяет от 0 до ... те буквы которые есть в пред аргументе
     System.out.println(b);
+    String belib = a+b;
+    belib = belib.substring(0,7);
+    System.out.println(belib);
+    String all = String.join ("#@",b,belib); // объединяет все строки, через знак
+    System.out.println(all);
+     if ("Evgen".equals(belib))
+         System.out.println(belib);
+                 else System.out.println(a); //сравнение строки на значение  "Evgen"
     }
 }
 
