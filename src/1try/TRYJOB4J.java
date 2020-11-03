4287c003362473fde82580429e57fb08e6de247e
glavs <glavstroi_e@mail.ru>
2020-11-03 22:45:25 +0400

    Проверка упражнений книги + разбор каждого для наработки опыта

---
+++ src/1try/TRYJOB4J.java
@@ -0,0 +1,38 @@
+public class TRYJOB4J {
+    public static void main ()
+    {
+        int [] week = new week [7] ;
+        week [0] = 1;
+        week [1] = 2;
+        week [2] = 3;
+        week [3] = 4;
+        week [4] = 5;
+        week [5] = 6;
+        week [6] = 7;
+
+        int h;
+        int s;
+
+        if (week [] <= 5)
+         {
+
+             if (h<= 8)
+             {
+                 s = h*10;
+             }
+             if (h>8)
+             {
+                 s = h*10 + (h-8)*15;
+             }
+         }
+         else if (h<= 8) {
+            {
+                s = h * 10 * 2;
+            }
+            if (h > 8) {
+                s = (h * 10 + (h - 8) * 15) * 2;
+            }
+
+        }
+    }
+}
