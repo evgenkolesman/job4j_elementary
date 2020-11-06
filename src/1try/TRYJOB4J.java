fcb635cab48a67e44b818e3978dd0b5130ca866d
glavs <glavstroi_e@mail.ru>
2020-11-06 10:33:39 +0400

    Проверка упражнений книги + разбор каждого для наработки опыта

--- src/1try/TRYJOB4J.java
+++ src/1try/TRYJOB4J.java
@@ -1,36 +1,80 @@
 public class TRYJOB4J {
-    public static void calculate  (int [] week)
-    {
-        weekHours [] week  = new weekHours[7];
-        for week [int x] = 1;
-        x++;
+    public static void main(String[] args) {
 
-        int n = week [];
+        weekHours[] week = new weekHours[7];
+
+        for (weekHours i : week)
+         i.weekHours (7);
+
+        for (weekHours i : week)
+            System.out.println(x.weekHours());
     }
 }
-
 class weekHours {
     int h;
     int s;
     int n;
-        if (n <= 5)
-    {
 
-        if (h<= 8)
+    public boolean weekHours(int x) {
+        if (n <= 5) {
+
+            if (h <= 8) {
+                s = h * 10;
+            }
+            if (h > 8) {
+                s = h * 10 + (h - 8) * 15;
+            }
+        }
+
+        else if (h <= 8)
         {
-            s = h*10;
+            {
+                s = h * 10 * 2;
+            }
+            if (h > 8) {
+                s = (h * 10 + (h - 8) * 15) * 2;
+            }
         }
-        if (h>8)
-        {
-            s = h*10 + (h-8)*15;
-        }
-    }
-         else if (h<= 8) {
-        {
-            s = h * 10 * 2;
-        }
-        if (h > 8) {
-            s = (h * 10 + (h - 8) * 15) * 2;
-        }
+    return x;
     }
 }
+/*package ru.job4j.condition;
+
+public class WeeklySalary
+{
+    public static int calculate(int[] hours)
+   {
+      hours = new int [7];
+      int s;
+      if (i<=4)
+      {
+
+        if (hours <= 8)
+        {
+                return s = hours  * 10;
+        }
+
+        if (h > 8)
+        {
+                return s = hours  * 10 + (hours  - 8) * 15;
+         }
+
+       }
+
+        if (i>4)
+        {
+          if (hours  <= 8)
+          {
+            return s = hours  * 10 * 2;
+            }
+
+            if (hours > 8)
+            {
+                return s = (hours  * 10 + (hours  - 8) * 15) * 2;
+            }
+
+        }
+
+      return 0;
+    }
+}*/
