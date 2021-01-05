a701f58d233ed4caac52ebf51bc6270ec3cd0112
glavs <glavstroi_e@mail.ru>
2021-01-05 23:20:55 +0300

    Проверка упражнений книги + разбор каждого для наработки опыта

---
+++ src/1try/arraylist/ArrayListTest.java
@@ -0,0 +1,22 @@
+/*package arraylist;
+import java.util.*;
+/** приминение класса ArrayList() */
+
+/*public class ArrayListTest
+{
+    public static void main (String []args)
+    {
+        // заполнить списочный массив staff тремя объектами типа Employee
+        staff.add (new Employee ("Carl", 75000,1987,12,15));
+        staff.add (new Employee ("Harry", 50000,1989,10,1));
+        staff.add (new Employee ("Tony", 40000, 1990,3,15));
+
+        // поднять всем работникам зарплату на 5%
+        for ( Employee e: staff)
+            e.raiseSalary(5);
+        // вывести данные обо всех объектах типа Employee
+        for (Employee e : staff)
+            System.out.println("name=" + e.getName()+ " , salary=" + e.getSalary() + ", hireDay=" + e.getHireDay());
+    }
+}
+**/
