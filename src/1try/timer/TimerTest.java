b8375681e9e4811f0d4b4d50da0b3066832a7e98
glavs <glavstroi_e@mail.ru>
2021-01-12 12:51:38 +0300

    Проверка упражнений книги + разбор каждого для наработки опыта

---
+++ src/1try/timer/TimerTest.java
@@ -0,0 +1,32 @@
+package timer;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+// имя класса указанно полностью для разрешения
+//конфликта имен с классом java.util.Timer
+
+
+
+
+public class TimerTest
+{
+    public static void main (String[]args)
+    {
+    ActionListener listener = new TimePrinter();
+    //построить таймер который вызывает приемник каждые 10 секунд
+    Timer t = new Timer(10000, listener);
+    t.start();
+    JOptionPane.showMessageDialog(null, "Quit programm?");
+    System.exit(0);
+    }
+}
+
+class TimePrinter implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        System.out.println("At the tone, the time is " + new Date());
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
