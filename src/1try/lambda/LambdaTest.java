47452a6203c1462843be5771f9dd3b2e2861f07c
glavs <glavstroi_e@mail.ru>
2021-01-15 14:36:22 +0300

    Проверка упражнений книги + разбор каждого для наработки опыта

---
+++ src/1try/lambda/LambdaTest.java
@@ -0,0 +1,31 @@
+package lambda;
+
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer ;
+
+//Применение лямбда-выражений
+
+public class LambdaTest
+{
+    public static void main (String[] args)
+    {
+        String [] planets = { "Меркурий", "Венера", "Земля",
+                "Марс", "Юпитер", "Сатурн", "Уран","Плутон"};
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Отсортированно в афавитной последовательности:");
+        Arrays.sort(planets);
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Отсортировано по длинне: ");
+        Arrays.sort(planets, (first, second) -> first.length()-second.length());
+        System.out.println(Arrays.toString(planets));
+
+        Timer t= new Timer(100000, event ->
+                System.out.println("Время: "+ new Date()));
+        t.start();
+        //выполнять программу до тех пор пока пользовательне кликнет "ОК"
+        JOptionPane.showMessageDialog(null, "Покинуть программу?");
+        System.exit(0);
+
+    }
+}
