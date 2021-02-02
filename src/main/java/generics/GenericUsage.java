b50aabc6c91f8c9bc2dd7f27a57ed7dca1773ef3
glavs <glavstroi_e@mail.ru>
2021-02-02 09:51:46 +0300

    Пробные работы

--- src/main/java/generics/GenericUsage.java
+++ src/main/java/generics/GenericUsage.java
@@ -1,19 +1,28 @@
 package generics;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class GenericUsage {
+
     public static void main(String[] args) {
         List list = new ArrayList();
         list.add("first");
         list.add("second");
         list.add("third");
-        list.add(new Person("name", 21, new Date(913716000000L));
+        list.add(new Person("name", 21, new Date(913716000000L)));
+        System.out.println("Количество элементов в списке: " + list.size());
         for (int i = 0; i < list.size(); i++) {
             String s = (String) list.get(i);
             System.out.println("Текущий элемент: " + s);
+            List<Integer> l = List.of(1, 2, 3, 4, 5);
+            new GenericUsage().printRsl(l);
+        }
+    }
+
+    public void printRsl(Collection<?> col) {
+        for (Iterator<?> it = col.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
         }
     }
 }
