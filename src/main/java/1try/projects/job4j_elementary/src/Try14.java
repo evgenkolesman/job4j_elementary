4b80fd43606779330f3e64e427ca209cb145681e
glavs <glavstroi_e@mail.ru>
2021-07-02 17:10:21 +0300

    Training

--- src/main/java/1try/projects/job4j_elementary/src/Try14.java
+++ src/main/java/1try/projects/job4j_elementary/src/Try14.java
@@ -1,14 +1,16 @@
+
+
 import projects.job4j_elementary.src.*;
 //импортируем класс Employee
 import static java.lang.System.*;
 
 public class Try14 {
-    public static void main (String []args)
-    {
+    public static void main(String[] args) {
         //вводим импортируемый класс без полного имени
-        Try15 harry = new Try15("Harry Hacker", 50000,1989,10,1);
-        harry.raiseSalary (5);
+        Try15 harry = new Try15("Harry Hacker", 50000, 1989, 10, 1);
+        harry.raiseSalary(5);
         // здесь не нужно писать полное System.out так как используется оператор static.import
-        out.println ("Имя = " + harry.getName() + ",значение = " + harry.getSalary());
+        out.println("Имя = " + harry.getName() + ",значение = " + harry.getSalary());
+        out.println(harry);
     }
 }
